0acaf1e847f1340320450b41c684b98b615beaf8
Joshua <oluferonmijoshua@gmail.com>
2022-06-19 18:55:42 +0100

    The firsttime

--- core/settings/base.py
+++ core/settings/base.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/pictures/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures/')
 django_heroku.settings(locals())
 
 
